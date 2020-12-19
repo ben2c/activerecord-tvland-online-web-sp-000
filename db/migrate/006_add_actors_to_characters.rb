dd5c83663fe2b25f64c41fa3d1c662c7e9707081
Benjamin Cheng <bencheng200@gmail.com>
2020-12-19 03:01:07 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_actors_to_characters.rb
+++ db/migrate/006_add_actors_to_characters.rb
@@ -1,4 +1,4 @@
-class AddActorsToCharacters < ActiveRecord::Migration
+class AddActorsToCharacters < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :actor_id, :integer
   end
